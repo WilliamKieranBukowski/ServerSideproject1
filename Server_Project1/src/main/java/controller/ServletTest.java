8c3ee7239d5f58e370f076fae9d3785fd068fbf9
William Bukowski <developerwilliamkbukowski@gmail.com>
2022-03-28 16:04:00 -0400

    adds class forName line in java, plus gradle dependency for postgresql

--- Server_Project1/src/main/java/controller/ServletTest.java
+++ Server_Project1/src/main/java/controller/ServletTest.java
@@ -1,6 +1,5 @@
 package controller;
 
-import DB.ConnectionService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,43 +7,51 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @WebServlet("/Server") //urlPatterns =
 public class ServletTest extends HttpServlet {
 
+    public static Connection c;
+
+    // "jdbc:<dialect>://<host name>:<port>/<database name>?currentSchema=<schema name>"
+    private static final String url = "jdbc:postgresql://postgres.ckheurx0twws.us-east-1.rds.amazonaws.com:5432/postgres"; //AWS link
+    private static final String username = "postgres";
+    private static final String password = "Bilbuk.1994";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
 
         //String jsonExample = "<h1>Test Response From Server: GET Succeeded</h1>";
         //resp.setContentType("text/html");
         //resp.setStatus(200); // we can set the status of the response
-        //resp.getOutputStream().println(jsonExample);
+        resp.getOutputStream().println("Testing client output...");
 
         //pull username data from userbank
-        try {
-            ConnectionService.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
         Statement st = null;
         ResultSet rs = null;
+
         try {
-            st = ConnectionService.c.createStatement();
+            Class.forName("org.postgresql.Driver"); //necessary, apparently
+            c = DriverManager.getConnection(url, username, password);
+            st = c.createStatement();
             rs = st.executeQuery("select * from testTable ;");
 
             while (rs.next()) {  //while there is record in row
                 String users = rs.getString("record1");
-                resp.getOutputStream().println(users);
+                try {
+                    resp.getOutputStream().println(users);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             if (st != null) {
-                System.out.print("Connection is closing");
+                resp.getOutputStream().println("Connection closed to db ");
                 try {
                     st.close();
                 } catch (SQLException e) {
@@ -52,6 +59,7 @@ public class ServletTest extends HttpServlet {
                 }
             }
         }
+
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
